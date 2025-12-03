b2cb5c431e82ed653353bb65b5f2ced6ccad7cd2
adm.celona.ext <massimo.celona@protiviti.it>
2025-12-03 14:35:48 +0100

    creazione conversione

--- Exchange.cs
+++ Exchange.cs
@@ -1,1 +1,8 @@
-
+// crea un funzione che converta dollari in Euro
+public class Exchange
+{
+    public static double ConvertDollarsToEuros(double dollars, double exchangeRate)
+    {
+        return dollars * exchangeRate;
+    }
+}
